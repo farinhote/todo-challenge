9ddc729e0768881f948c8f1d54cac8b8ef59926b
Farinhote <andrefarinhote@gmail.com>
2020-02-14 15:49:23 +0000

    First fully functional service of new approach
    Also added package.json :D

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,20 @@ const bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(logger('dev'));
+app.set('secretKey', 'nodeRestApi'); // jwt secret token
+
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.get('/', function (req, res) {
+  res.json({ "tutorial": "Build REST API with node.js" });
+});
+
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
@@ -13,51 +27,39 @@ app.all('*', function (req, res, next) {
   next();
 });
 
-app.set('secretKey', 'nodeRestApi'); // jwt secret token
-// connection to mongodb
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', function (req, res) {
-  res.json({ "tutorial": "Build REST API with node.js" });
-});
-
-
 app.use('/users', users);
 
-app.get('/favicon.ico', function(req, res) {
-    res.sendStatus(204);
+app.get('/favicon.ico', function (req, res) {
+  res.sendStatus(204);
 });
 
 function validateUser(req, res, next) {
-  jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function(err, decoded) {
+  jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function (err, decoded) {
     if (err) {
-      res.json({status:"error", message: err.message, data:null});
-    }else{
+      res.json({ status: "error", message: err.message, data: null });
+    } else {
       // add user id to request
       req.body.userId = decoded.id;
       next();
     }
   });
-  
+
 }
 
-app.use(function(req, res, next) {
- let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // handle errors
-app.use(function(err, req, res, next) {
- console.log(err);
- 
-  if(err.status === 404)
-   res.status(404).json({message: "Not found"});
-  else 
-    res.status(500).json({message: "Something went wrong"});
+app.use(function (err, req, res, next) {
+  console.log(err);
+
+  if (err.status === 404)
+    res.status(404).json({ message: "Not found" });
+  else
+    res.status(500).json({ message: "Something went wrong" });
 });
 
 app.listen(3000, function () { console.log('Node server listening on port 3000'); });
